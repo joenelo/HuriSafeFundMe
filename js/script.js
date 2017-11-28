f08449b8d67555e914dbe79fc5032d7b32c7da86
joean <joeandrewnelson@gmail.com>
2017-11-28 00:00:03 -0800

    Initial commit Recommit of completed project for Web Design 4.

---
+++ js/script.js
@@ -0,0 +1,168 @@
+//LOADER/SPINNER
+$(window).bind("load", function() {
+
+    "use strict";
+    
+    $(".spn_hol").fadeOut(1000);
+});
+
+
+// FIX HOME SCREEN HEIGHT
+$(document).ready(function() {
+
+    "use strict";
+    
+    setInterval(function() {
+
+        "use strict";
+        
+        var widnowHeight = $(window).height();
+        var containerHeight = $(".home-container").height();
+        var padTop = widnowHeight - containerHeight;
+        $(".home-container").css({
+            'padding-top': Math.round(padTop / 2) + 'px',
+            'padding-bottom': Math.round(padTop / 2) + 'px'
+        });
+    }, 10)
+});
+
+
+
+//PARALLAX
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(window).bind('load', function() {
+        "use strict";
+        parallaxInit();
+    });
+
+    function parallaxInit() {
+        "use strict";
+        $('.home-parallax').parallax("30%", 0.1);
+        $('.subscribe-parallax').parallax("30%", 0.1);
+        $('.testimonial').parallax("10%", 1);
+        /*add as necessary*/
+    }
+});
+
+
+
+//RESPONSIVE VIDEO
+$(document).ready(function() {
+
+    "use strict";
+    
+    // Basic FitVids Test
+    $(".video").fitVids();
+});
+
+
+
+//CONTACT FORM VALIDATION
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(".form_submit").click(function() {
+
+        "use strict";
+        
+        var name = $("#name").val();
+        var emaild = $("#email").val();
+        var subject = $("#subject").val();
+        var message = $("#message").val();
+        var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+        if (!name) {
+            $(".form_error .name_error").addClass("show").removeClass("hide");
+            return false;
+        } else {
+            $(".form_error .name_error").addClass("hide").removeClass("show");
+        }
+        if (!emaild) {
+            $(".form_error .email_error").addClass("show").removeClass("hide");
+            return false;
+        } else {
+            $(".form_error .email_error").addClass("hide").removeClass("show");
+            if (testEmail.test(emaild)) {
+                $(".form_error .email_val_error").addClass("hide").removeClass("show");
+            } else {
+                $(".form_error .email_val_error").addClass("show").removeClass("hide");
+                return false;
+            }
+        }
+        if (!message) {
+            $(".form_error .message_error").addClass("show").removeClass("hide");
+            return false;
+        } else {
+            $(".form_error .message_error").addClass("hide").removeClass("show");
+        }
+        if (name && emaild && message) {
+            $.ajax({
+                url: 'contact.php',
+                data: {
+                    name: name,
+                    emaild: emaild,
+                    subject: subject,
+                    message: message
+                },
+                type: 'POST',
+                success: function(data) {
+                    $(".Sucess").show();
+                    $(".Sucess").fadeIn(2000);
+                    $(".Sucess").html("<i class='fa fa-check'></i> Dear <b>" + name + "</b> Thank you for your inquiry we will respond to you as soon as possible!");
+                    $("#Name").val("");
+                    $("#Email").val("");
+                    $("#donate").val("");
+                    $("#Message").val("");
+                    $(".form_error .name_error, .form_error .email_error, .form_error .email_val_error, .form_error .message_error").addClass("hide").removeClass("show");
+                    $("#name").val("");
+                    $("#email").val("");
+                    $("#subject").val("");
+                    $("#message").val("");
+                }
+            });
+        }
+        return false;
+    });
+});
+
+
+ 
+/// SMOOTH SCROLL           
+
+$(document).ready(function() {
+
+    "use strict";
+    
+    var scrollAnimationTime = 1200,
+        scrollAnimation = 'easeInOutExpo';
+    $('a.scrollto').bind('click.smoothscroll', function(event) {
+        event.preventDefault();
+        var target = this.hash;
+        $('html, body').stop().animate({
+            'scrollTop': $(target).offset().top
+        }, scrollAnimationTime, scrollAnimation, function() {
+            window.location.hash = target;
+        });
+    });
+    //COUNTER
+    $('.counter_num').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+
+//VIDEO BACKGROUND
+$(document).ready(function() {
+  var videobackground = new $.backgroundVideo($('body'), {
+    "align": "centerXY",
+    "width": 1280,
+    "height": 720,
+    "path": "media/",
+    "filename": "cloud",
+    "types": ["mp4","ogg","webm"]
+  });
+});
